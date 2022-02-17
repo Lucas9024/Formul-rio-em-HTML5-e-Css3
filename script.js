644b38ab457aa9115d01cd01e064488143a1416f
Lucas Ferreira <lucassfrs9080@gmail.com>
2022-02-17 00:57:41 -0300

    Commit dos meus arquivos html e css

---
+++ script.js
@@ -0,0 +1,10 @@
+
+//alerta de dados salvos 
+
+const save = document.getElementById("save")
+
+function salvar(){
+    alert("Seus dados foram salvos com sucesso!")
+}
+
+save.addEventListener("click", salvar)
